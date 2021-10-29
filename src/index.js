d10f89869bffc6800f2ebcd34288ff1700749fd0
sakayoshi614 <93253382+sakayoshi614@users.noreply.github.com>
2021-10-29 14:39:29 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  // テキスト取得して初期化
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  console.log(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
